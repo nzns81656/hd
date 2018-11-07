38697010a7f948549e479980967764e491b4dc4b
unknown <8350381@qq.com>
2018-11-07 14:23:13 +0800

    map

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/view/login/index'
+import map from '@/view/login/map'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login/index',
       name: 'index',
       component: index
+    },
+    {
+      path: '/login/map',
+      name: 'map',
+      component: map
     }
   ]
 })
---
+++ src/view/Login/map.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+     
+  <cube-page  title="地图" class="option-demo">
+    <div slot="content">
+        <baidu-map center="北京" style="height:100vh;width:100%;padding-top:-60px" :zoom="15"></baidu-map>
+  </div>   
+  </cube-page>
+  </div>
+</template>
+<script src="https://unpkg.com/vue-baidu-map"></script>
+<script type="text/ecmascript-6">
+import cubePage from "../../components/cube-page";
+export default {
+  components: {
+    cubePage
+  },
+  data() {
+    return {
+      mwidth:'100%',
+      mheight:'100vh',
+      pdt:'-60px',
+      center: {lng: 0, lat: 0},
+      zoom: 3,
+      mapStyle: {
+        styleJson: [
+          {
+            "featureType": "all",
+            "elementType": "geometry",
+            "stylers": {
+                "hue": "#007fff",
+                "saturation": 89
+            }
+          },
+          {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": {
+                "color": "#ffffff"
+            }
+          }
+        ]
+      }
+    };
+  },
+  created(){
+　　//this.mheight = window.innerHeight-64+'px';
+  },
+  computed: {
+    
+  },
+  methods: {
+   
+  },
+  mounted() {
+  }
+};
+</script>
+<style>
+   
+</style>
